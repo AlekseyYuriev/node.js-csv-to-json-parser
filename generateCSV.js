4be32953dda9086106179b0daa01b2aa5a18d15b
Aleksey Yuriev <alex11-90@yandex.by>
2024-10-30 22:26:27 +0300

    feat/add-generateCSV-file

---
+++ generateCSV.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const readline = require("readline");
+
+function generateRandomNumberOfRows(separator, numberOfColumns, numberOfRows) {
+  let row = "";
+  for (let i = 0; i < numberOfColumns; i++) {
+    row += `veryLongRandomWord${separator}`;
+  }
+  return (row + "\n").repeat(numberOfRows);
+}
+
+async function readFirstLine(fileName) {
+  const readStream = fs.createReadStream(fileName);
+
+  const rl = readline.createInterface({
+    input: readStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    rl.close();
+    readStream.close();
+    return line;
+  }
+}
+
+readFirstLine("CSV_Files/mock.csv").then((data) => {
+  const largeCSVFile = "CSV_Files/large.csv";
+  const separator = ",";
+  const numberOfColumns = data.split(separator).length;
+  const randomNumberOfRows = generateRandomNumberOfRows(
+    separator,
+    numberOfColumns,
+    1000
+  );
+
+  fs.writeFileSync(largeCSVFile, data + "\n");
+
+  let fileSize = fs.statSync(largeCSVFile).size;
+
+  while (fileSize < 10 * 1024 * 1024 * 1024) {
+    fs.appendFileSync(largeCSVFile, randomNumberOfRows);
+    fileSize = fs.statSync(largeCSVFile).size;
+  }
+});
